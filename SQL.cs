e9a81e1f3b80b1fc4a4ef889fe99ce2bd3be1e8f
EddieStruzberg <50948434+EddieStruzberg@users.noreply.github.com>
2019-05-28 20:37:49 +0300

    Add files via upload

---
+++ SQL.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class SQL
+    {
+        static void Main(string[] args)
+        {
+            RunDBQuary();
+        }
+        public static void RunDBQuary()
+        {
+            string server = @"EDDIES-PC\SQLEXPRESS";
+            string username = "eddie";
+            string password = "aaa";
+            //string connectionString = @"Trusted_Connection=True; Server=" + server + "; Data Source=localhost; Initial Catalog=NLHospital; Integrated Security=false;user id=" + username + ";password=" + password;
+            string connectionString  = @"Trusted_Connection=True;Server=" + server;
+
+            var sql = "INSERT INTO[FootballStats].[dbo].[MatchesStats]  (Matchdate,PassPercentFlow, PassTotalFlow )  Values(@aaa,@bbb,@ccc,)";
+            var sql2 = "SELECT count(a.[Email]) as orderscount FROM[AppDB].[dbo].[User] a left join[AppDB].[dbo].[orders] b on a.Email = b.Email group by a.[Email] ";
+
+
+            string Output = "";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(sql, connection);
+
+                connection.Open();
+                cmd.CommandTimeout = 60; // timeout given for query, if will exceed, will fail
+
+                //var reader = cmd.ExecuteScalar();
+                var reader = cmd.ExecuteReader();
+                if (reader != null)
+                {
+                    Output = reader.ToString();
+                }
+
+                connection.Close();
+            }
+        }
+        public void createtable()
+        {
+            //USE[AppDB]
+            //GO
+            //SET ANSI_NULLS ON
+            //GO
+            //CREATE TABLE Orders(
+            //id int,
+            //Email varchar(255),
+            //orderid varchar(255)
+            //);
+        }
+    }
+}
